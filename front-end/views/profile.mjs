b5d3cd71563c898d36614ae08283ee007c434cf8
pathywang <pathywang@gmail.com>
2026-09-18 22:52:48 +0100

    fix bug

--- front-end/views/profile.mjs
+++ front-end/views/profile.mjs
@@ -38,9 +38,8 @@ function profileView(username) {
     "login-template",
     createLogin
   );
-  document
-    .querySelector("[data-action='login']")
-    ?.addEventListener("click", handleLogin);
+  const form = document.querySelector("[data-form='login']");
+  form?.addEventListener("submit", handleLogin);
 
   const profileData = state.profiles.find((p) => p.username === username);
   if (profileData) {
